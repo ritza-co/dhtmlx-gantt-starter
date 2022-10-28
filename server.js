01416a3bcc8fb64266fa8bbb6aa0a5fd159c58b9
Matthew Clarke <m_d_clarke@hotmail.com>
2022-10-28 11:55:42 +0200

    small fix

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ const mysql = require("mysql2/promise");
 
 async function serverСonfig() {
   const db = mysql.createPool({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
   });
 
   function getTask(data) {
